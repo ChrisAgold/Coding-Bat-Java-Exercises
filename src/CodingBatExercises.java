bfdfc4aa9f6b896d6fe0d5e28f297d68a989f34b
Chris Agold <christophersagold@gmail.com>
2021-01-28 08:21:48 -0600

    completed warmup 2

--- src/CodingBatExercises.java
+++ src/CodingBatExercises.java
@@ -10,7 +10,27 @@ public class CodingBatExercises {
         if (!weekday || vacation) {
             return true;
         }
-
         return false;
+
+        // Warmup 2
+//        Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+//        diff21(19) → 2
+//        diff21(10) → 11
+//        diff21(21) → 0
+        // Solution
+//        public int diff21(int n) {
+//            if(n > 21) {
+//                return Math.abs(2*(n-21));
+//            } else {
+//                return Math.abs(n - 21);
+//            }
+//        }
     }
+
+
+
+
+
+
+
 }
