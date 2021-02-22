51dda33fab5ad2cf0e9c78b37891afbed69ad38a
Chris Agold <christophersagold@gmail.com>
2021-02-21 20:18:31 -0600

    completed last digit

--- src/CodingBatExercises.java
+++ src/CodingBatExercises.java
@@ -67,8 +67,11 @@ public class CodingBatExercises {
         return(two.equals("ix"));
     }
 
-    // close10
-    // Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
-
+    // lastDigit
+    // Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+    public boolean lastDigit(int a, int b) {
+        // True if the last digits are the same
+        return(a % 10 == b % 10);
+    }
 
 }
